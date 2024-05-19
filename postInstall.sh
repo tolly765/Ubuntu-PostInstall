414f7d553b790dd71c0c7cf749a5e1f02fdec4df
Tom <tolly765@gmail.com>
2024-05-19 19:32:19 +0100

    Re-theming root and logged in user
    - Restructure to fix "curl was not found"
    - Route output of unzip to dev/null
    - Set zsh and reboot to apply changes

--- postInstall.sh
+++ postInstall.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
-# Install ZSH and OMZ
+# Download common dependencies
 sudo apt update
+sudo apt upgrade -y
+sudo apt install python3-pip -y
+sudo apt install curl -y
+
+# Install ZSH and OMZ
 sudo apt install zsh -y
-chsh -s "$(which zsh)"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+sudo chsh -s "$(which zsh)" "$USER" # Change shell for current user
+sudo chsh -s "$(which zsh)" root    # Change shell for root
+zsh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # Download theme
-curl https://raw.githubusercontent.com/tolly765/Custom-ZSH/main/.oh-my-zsh/themes/kali-like.zsh-theme --create-dirs -o ~/.oh-my-zsh/themes/kali-like.zsh-theme
+curl https://raw.githubusercontent.com/tolly765/Ubuntu-PostInstall/main/.oh-my-zsh/themes/kali-like.zsh-theme -o ~/.oh-my-zsh/themes/kali-like.zsh-theme
 
 # Download zshrc
-curl https://raw.githubusercontent.com/tolly765/Custom-ZSH/main/.zshrc -o ~/.zshrc
+curl https://raw.githubusercontent.com/tolly765/Ubuntu-PostInstall/main/.zshrc -o ~/.zshrc
 
-# Download common dependencies
-sudo apt install python3-pip
+#? ROOT SECTION
+sudo /usr/bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+sudo /bin/sh -c "curl https://raw.githubusercontent.com/tolly765/Ubuntu-PostInstall/main/.oh-my-zsh/themes/kali-like.zsh-theme -o ~/.oh-my-zsh/themes/kali-like.zsh-theme"
+sudo /bin/sh -c "curl https://raw.githubusercontent.com/tolly765/Ubuntu-PostInstall/main/.zshrc -o ~/.zshrc"
 
 # Install awscli
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
+unzip awscliv2.zip >/dev/null
 sudo ./aws/install
+
+sudo reboot now
